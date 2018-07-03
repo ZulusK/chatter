45f208a16c176ff02df979f301080323f9bfe09d
Danil Kazimirov <danil.kazim.99@gmail.com>
2018-07-04 02:53:04 +0300

    change script ext to sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-if [[ "$TRAVIS_BRANCH" != "master" ]]; then
-    echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-    docker build -t registry.heroku.com/chatter/web .
-    docker push registry.heroku.com/chatter/web
-fi
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+docker build -t registry.heroku.com/chatter/web .
+docker push registry.heroku.com/chatter/web
 
